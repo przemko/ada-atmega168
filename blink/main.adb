8f5ef81eadfd8a1174586a95dd97098eef80dcc6
Przemyslaw Kobylanski <przemko@mac.com>
2018-03-21 12:56:29 +0100

    dodano przykład blink

---
+++ blink/main.adb
@@ -0,0 +1,23 @@
+-- main.adb
+
+with AVR; use AVR;
+with AVR.MCU;
+with AVR.Real_Time.Delays; pragma Unrefered (AVR.Real_Time.Delays);
+
+procedure Main is
+
+   LED : Boolean renames MCU.PortB_Bits (0);
+
+begin
+   MCU.CLKPR := 16#80#; -- pozwala przez 4 cykle wpisac dzielnik
+   MCU.CLKPR := 16#00#; -- wpisuje dzielnik rowny 1
+
+   MCU.DDRB_Bits := (others => DD_Output);
+
+   loop
+      LED := High;
+      delay 0.1; 
+      LED := Low;
+      delay 0.1;
+   end loop;
+end Main;
